53ec5a9abeac0d41df53f6baba114e4c06d85618
Edmundas <edmundas@andriuska.eu>
2025-03-05 20:32:13 +0200

    init 6

--- main.tf
+++ main.tf
@@ -1,14 +1,37 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    proxmox = {
+      source = "danitso/Proxmox"
+      version = "2.8.0"
     }
   }
 }
 
-provider "aws" {
-  region = var.aws_region
+# Define variables
+variable "proxmox_password" {
+  description = "Proxmox API password"
+  type        = string
+}
+
+provider "proxmox" {
+  pm_api_url = "https://192.168.0.3:8006/api2/json"
+  pm_user    = "root@pam"
+  pm_password = var.proxmox_password
+  pm_tls_insecure = true
+}
+
+resource "proxmox_lxc" "example_lxc" {
+  hostname    = "my-lxc-container"
+  ostemplate  = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"  # You can change this to your desired OS template
+  storage     = "local"
+  memory      = 512  # Memory in MB
+  cores       = 1     # Number of CPU cores
+  netif {
+    name    = "eth0"
+    bridge  = "vmbr0"
+    ip      = "dhcp"
+  }
+  rootfs      = "local:2"  # Define storage and size for rootfs (local:2 means 2GB)
+  password    = var.proxmox_password
+  swap        = 128   # Swap in MB
 }
